d04fd945642ea2442ecad9e9401b71edc4d103f0
Robert Rodini <rrodini@hotmail.com>
2019-09-19 14:52:37 -0400

    Undo this change - require ../player.html from player

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 app.use('/projector', express.static(path.join(__dirname, 'public')));
 app.use('/startGame', startGameRouter);
 app.use('/emcee', emceeRouter);
-app.use('/player', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
